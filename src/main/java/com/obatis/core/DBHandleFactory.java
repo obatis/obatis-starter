1397f473f27c5b3236a824dbda172533e6951655
huanglongpu <306315964@qq.com>
2019-05-10 16:50:13 +0800

    优化名称

--- src/main/java/com/obatis/core/DBHandleFactory.java
+++ src/main/java/com/obatis/core/DBHandleFactory.java
@@ -140,17 +140,17 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 
 	/**
 	 * 传入数据库封装操作对象 QueryProvider，进行更新
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public int update(QueryProvider queryProvider) throws HandleException {
+	public int update(QueryProvider provider) throws HandleException {
 		
-		if(queryProvider == null) {
+		if(provider == null) {
 			throw new HandleException("error: update QueryProvider is null");
 		}
 		
 		Map<String, Object> paramMap = new HashMap<>();
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().update(paramMap, this.getTableName());
 	}
 
@@ -182,12 +182,12 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 
 	/**
 	 * 根据传入的 QueryProvider 对象，进行删除操作
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public int delete(QueryProvider queryProvider) throws HandleException {
+	public int delete(QueryProvider provider) throws HandleException {
 		Map<String, Object> paramMap = new HashMap<>();
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().delete(paramMap, this.getTableName());
 	}
 
@@ -219,120 +219,120 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 	/**
 	 * 1、根据id主键查询一条记录，返回设定的字段。
 	 * 2、如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @param id
 	 * @return
 	 */
-	public T findById(QueryProvider queryProvider, BigInteger id) {
-		queryProvider.equals(CommonField.FIELD_ID, id);
-		return this.find(queryProvider);
+	public T findById(QueryProvider provider, BigInteger id) {
+		provider.equals(CommonField.FIELD_ID, id);
+		return this.find(provider);
 	}
 
 	/**
 	 * 1、根据id主键查询一条记录，返回设定的字段，返回类型为预设的class类型，需强制转换一次。
 	 * 2、如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @param id
 	 * @param resultCls
 	 * @return
 	 */
-	public <M> M findById(QueryProvider queryProvider, BigInteger id, Class<M> resultCls) {
-		queryProvider.equals(CommonField.FIELD_ID, id);
-		return this.find(queryProvider, resultCls);
+	public <M> M findById(QueryProvider provider, BigInteger id, Class<M> resultCls) {
+		provider.equals(CommonField.FIELD_ID, id);
+		return this.find(provider, resultCls);
 	}
 
 	/**
 	 * 1、根据传入的 QueryProvider 对象，查询一条 CommonModel 子类的记录。
 	 * 2、如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public T find(QueryProvider queryProvider) {
+	public T find(QueryProvider provider) {
 		Map<String, Object> providerMap = new HashMap<>();
-		providerMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		providerMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().find(providerMap, this.getTableName());
 	}
 
 	/**
 	 * 1、根据传入的 QueryProvider 对象，返回类型为预设的class类型，需强制转换一次。
 	 * 2、如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @param resultCls
 	 * @return
 	 */
-	public <M> M find(QueryProvider queryProvider, Class<M> resultCls) {
+	public <M> M find(QueryProvider provider, Class<M> resultCls) {
 		Map<String, Object> providerMap = new HashMap<>();
-		providerMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		providerMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseResultSessionMapper(resultCls).findR(providerMap, this.getTableName());
 	}
 	
 	/**
-	 * 1、主要作用为校验，queryProvider 只需传入条件值即可，映射的SQL语句例如：select count(1) from test t where t.name='test';
+	 * 1、主要作用为校验，provider 只需传入条件值即可，映射的SQL语句例如：select count(1) from test t where t.name='test';
 	 * 2、根据 count 函数的返回值进行判断，返回值大于0表示存在，否则不存在。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public boolean validate(QueryProvider queryProvider) {
+	public boolean validate(QueryProvider provider) {
 		Map<String, Object> providerMap = new HashMap<>();
-		providerMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		providerMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().validate(providerMap, this.getTableName()) > 0;
 	}
 
 	/**
 	 * 根据传入的 QueryProvider 对象，返回一条Map格式记录。 如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public Map<String, Object> findConvertMap(QueryProvider queryProvider) {
+	public Map<String, Object> findConvertMap(QueryProvider provider) {
 		Map<String, Object> providerMap = new HashMap<>();
-		providerMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		providerMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().findToMap(providerMap, this.getTableName());
 	}
 
 	/**
 	 * 根据传入的 QueryProvider 对象，返回符合条件的list集合的BaseEntity记录。
 	 * 如果有传入分页标识，只返回设置页面的极限值，否则返回所有符合条件的数据。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public List<T> list(QueryProvider queryProvider) {
+	public List<T> list(QueryProvider provider) {
 		Map<String, Object> paramMap = new HashMap<>();
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().list(paramMap, this.getTableName());
 	}
 
 	/**
 	 * 根据传入的 QueryProvider 对象，返回符合条件的list集合，返回类型为预设的class类型，需强制转换一次。
 	 * 如果有传入分页标识，只返回设置页面的极限值，否则返回所有符合条件的数据。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public <M> List<M> list(QueryProvider queryProvider, Class<M> resultCls) {
+	public <M> List<M> list(QueryProvider provider, Class<M> resultCls) {
 		Map<String, Object> paramMap = new HashMap<>();
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseResultSessionMapper(resultCls).listR(paramMap, this.getTableName());
 	}
 
 	/**
 	 * 根据传入的 QueryProvider 对象，返回符合条件的List集合的Map格式记录。
 	 * 如果有传入分页标识，只返回设置页面的极限值，否则返回所有符合条件的数据。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public List<Map<String, Object>> listConvertMap(QueryProvider queryProvider) {
+	public List<Map<String, Object>> listConvertMap(QueryProvider provider) {
 		Map<String, Object> paramMap = new HashMap<>();
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().query(paramMap, this.getTableName());
 	}
 
 	/**
 	 * 根据传入的 QueryProvider 对象，返回BigDecimal的类型值。 该方法常用于查询金额字段。
 	 * 如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public BigDecimal findBigDecimal(QueryProvider queryProvider) {
-		Object obj = this.findObject(queryProvider);
+	public BigDecimal findBigDecimal(QueryProvider provider) {
+		Object obj = this.findObject(provider);
 		if (obj != null) {
 			return new BigDecimal(obj.toString());
 		}
@@ -342,11 +342,11 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 	/**
 	 * 根据传入的 QueryProvider 对象，返回int的类型值。 该方法常用于查询count等类型的业务。
 	 * 如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public int findInt(QueryProvider queryProvider) {
-		Object obj = this.findObject(queryProvider);
+	public int findInt(QueryProvider provider) {
+		Object obj = this.findObject(provider);
 		if (obj != null) {
 			return Integer.valueOf(obj.toString());
 		}
@@ -355,11 +355,11 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 
 	/**
 	 * 根据传入的 QueryProvider 对象，返回Double的类型值。 如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	public Double findDouble(QueryProvider queryProvider) {
-		Object obj = this.findObject(queryProvider);
+	public Double findDouble(QueryProvider provider) {
+		Object obj = this.findObject(provider);
 		if (obj != null) {
 			return Double.valueOf(obj.toString());
 		}
@@ -369,12 +369,12 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 	/**
 	 * 根据传入的 QueryProvider 对象，返回Object的类型值。
 	 * 如果根据条件有多条数据符合，则抛出异常。
-	 * @param queryProvider
+	 * @param provider
 	 * @return
 	 */
-	private Object findObject(QueryProvider queryProvider) {
+	private Object findObject(QueryProvider provider) {
 		Map<String, Object> paramMap = new HashMap<>();
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		return this.getBaseBeanSessionMapper().findObject(paramMap, this.getTableName());
 	}
 
@@ -536,12 +536,12 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 	/**
 	 * 主要实现于在前端查询时选中的页面超过总条数，非前端分页查询，不建议使用。
 	 * 分页查询，同时返回分页数据和总条数。
-	 * @param queryProvider 封装的参数对象
+	 * @param provider 封装的参数对象
 	 * @return
 	 */
-	public PageInfo<T> page(QueryProvider queryProvider) {
+	public PageInfo<T> page(QueryProvider provider) {
 		Map<String, Object> providerMap = new HashMap<>();
-		providerMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		providerMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		// 拼装SQL语句
 		SqlHandleProvider.getQueryPageSql(providerMap, this.getTableName());
 
@@ -554,7 +554,7 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 		}
 
 		String querySql = (String) providerMap.get(SqlConstant.PROVIDER_QUERY_SQL);
-		providerMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		providerMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		page.setList(this.getBaseBeanSessionMapper().page(querySql, providerMap));
 		return page;
 	}
@@ -562,13 +562,13 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 	/**
 	 * 主要实现于在前端查询时选中的页面超过总条数，非前端分页查询，不建议使用。
 	 * 分页查询，同时返回分页数据和总条数。
-	 * @param queryProvider  封装的参数对象
+	 * @param provider  封装的参数对象
 	 * @param resultCls      返回 预定义的 resultCls Bean 泛型数据类型
 	 * @return 
 	 */
-	public <M> PageInfo<M> PageResultHandle(QueryProvider queryProvider, Class<M> resultCls) {
+	public <M> PageInfo<M> PageResultHandle(QueryProvider provider, Class<M> resultCls) {
 		Map<String, Object> paramMap = new HashMap<>();
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		// 拼装SQL语句
 		SqlHandleProvider.getQueryPageSql(paramMap, this.getTableName());
 
@@ -582,7 +582,7 @@ public abstract class DBHandleFactory<T extends CommonModel> {
 		}
 
 		String querySql = (String) paramMap.get(SqlConstant.PROVIDER_QUERY_SQL);
-		paramMap.put(SqlConstant.PROVIDER_OBJ, queryProvider);
+		paramMap.put(SqlConstant.PROVIDER_OBJ, provider);
 		page.setList(this.getBaseResultSessionMapper(resultCls).pageR(querySql, paramMap));
 		return page;
 	}
