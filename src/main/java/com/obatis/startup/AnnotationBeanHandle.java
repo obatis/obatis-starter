25a0fddeba4798a93d8f1df9a33d2f9738904c93
huanglongpu <306315964@qq.com>
2019-05-30 17:40:32 +0800

    优化命名，之前命名出现英文拼写错误

--- src/main/java/com/obatis/startup/AnnotationBeanHandle.java
+++ src/main/java/com/obatis/startup/AnnotationBeanHandle.java
@@ -4,22 +4,22 @@ import com.obatis.config.SystemConstant;
 import com.obatis.core.annotation.Table;
 import com.obatis.core.convert.BeanCacheConvert;
 import org.reflections.Reflections;
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
-@Component
-@Order(0)
-public class AnnotationBeanHandle implements ApplicationListener<ContextRefreshedEvent> {
+@Configuration
+@Order(-99999)
+//public class AnnotationBeanHandle implements ApplicationListener<ContextRefreshedEvent> {
+public class AnnotationBeanHandle {
 
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent event) {
+	@Bean
+//	public int onApplicationEvent(ContextRefreshedEvent event) {
+	public int onApplicationEvent() {
 
-		if(event.getApplicationContext() instanceof AnnotationConfigServletWebServerApplicationContext) {
+//		if(event.getApplicationContext() instanceof AnnotationConfigServletWebServerApplicationContext) {
 			Reflections reflections = new Reflections(SystemConstant.PROJECT_BASE_DIR);
 			/**
 			 * 将注解的表加载到缓存
@@ -28,6 +28,7 @@ public class AnnotationBeanHandle implements ApplicationListener<ContextRefreshe
 			for (Class<?> cls : classList) {
 				BeanCacheConvert.loadEntityCache(cls);
 			}
-		}
+//		}
+		return 0;
 	}
 }
