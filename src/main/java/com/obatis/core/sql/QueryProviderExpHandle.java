fbeb5fcfac18bdd2dfb9480e5003fd8954855ccd
huanglongpu <306315964@qq.com>
2019-09-27 19:32:48 +0800

    ==

--- src/main/java/com/obatis/core/sql/QueryProviderExpHandle.java
+++ src/main/java/com/obatis/core/sql/QueryProviderExpHandle.java
@@ -12,7 +12,7 @@ public class QueryProviderExpHandle {
     }
 
     public QueryProviderExpHandle nullToZero() {
-        exp.insert(0, "ifnull(" + exp + ", 0)");
+        exp.insert(0, "ifnull((" + exp + "), 0)");
         return this;
     }
 
